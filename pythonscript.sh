0ee43fc6dc5f9946896a2f305a735066b96a356d
Suhan Dharmasuriya <suhanr@wso2.com>
2014-10-16 14:18:58 +0530

    cleaned

--- pythonscript.sh
+++ pythonscript.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 # Install python git project requirements
 echo "---- Installing git python web application project requirements ----"
-#pip install -r /var/www/www/requirements.txt
-pip install flask cherrypy
+pip install -r /var/www/www/requirements.txt
 echo "---- Starting CherryPy Server ----"
 /usr/bin/python /var/www/www/server.py > /dev/null 2>&1 &
 echo "---- past server.py ----"
