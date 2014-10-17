51cc519634aef532349997cd11102a7dabd43d4a
Suhan Dharmasuriya <suhanr@wso2.com>
2014-10-17 15:08:15 +0530

    scripts modified
    pythonscript.sh - details added
    cartridge definition files added

--- pythonscript.sh
+++ pythonscript.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
+
+# wait until the git repo to be cloned to /var/www/www/ directory
+while [ ! -f /var/www/www/requirements.txt ]
+do
+  echo "---- git repo is not cloned to /var/www/www/ yet, going to sleep, will recheck in 2 seconds ----"
+  sleep 2
+done
+
 # Install python git project requirements
-echo "---- Installing git python web application project requirements ----"
+echo "------------------ Installing git python web application project requirements ------------------"
+
+# retrieve python project specific dependencies from the project requirements.txt file
 pip install -r /var/www/www/requirements.txt
-echo "---- Starting CherryPy Server ----"
-/usr/bin/python /var/www/www/server.py > /dev/null 2>&1 &
-echo "---- past server.py ----"
+
+# Start CherryPy server on specified port
+# e.g.: port 5000
+# 		to work in port 5000 you have to modify Docker file exposing port 5000 and
+#		update the cartridge configuration accordingly http port -> 5000 and
+#		modify server.py server.socket_port -> 5000
+echo "----------------------------------- Starting CherryPy Server -----------------------------------"
+python /var/www/www/server.py > /tmp/server.log 2>&1 &
+echo "--------------- CherryPy server started... See /tmp/server.log for more details ----------------"
